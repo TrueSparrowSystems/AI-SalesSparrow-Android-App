17248d07676ce102d87463f1e19f80e464214cae
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-08-08 17:45:43 +0530

    Enhanced splash screen UI by replacing it with a clearer GIF.

--- app/src/main/java/com/example/salessparrow/screens/SplashScreen.kt
+++ app/src/main/java/com/example/salessparrow/screens/SplashScreen.kt
@@ -5,12 +5,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
@@ -18,40 +15,35 @@ import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import com.example.salessparrow.R
 import com.example.salessparrow.services.NavigationService
+import com.example.salessparrow.ui.theme.nero
 import com.example.salessparrow.util.Screens
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
     val isUserLoggedIn = true;
-    val gradientBrush = Brush.horizontalGradient(
-        colors = listOf(
-            Color(0xFF1E1E1E), Color(0xFF1E1E1E)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(nero)
+    ) {
+
+        val imageLoader = ImageLoader.Builder(LocalContext.current).components {
+            if (SDK_INT >= 28) {
+                add(ImageDecoderDecoder.Factory())
+            } else {
+                add(GifDecoder.Factory())
+            }
+        }.build()
+
+        Image(
+            painter = rememberAsyncImagePainter(R.drawable.splash_screen, imageLoader),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
         )
-    )
-    MaterialTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(brush = gradientBrush)
-        ) {
-
-            val imageLoader = ImageLoader.Builder(LocalContext.current).components {
-                    if (SDK_INT >= 28) {
-                        add(ImageDecoderDecoder.Factory())
-                    } else {
-                        add(GifDecoder.Factory())
-                    }
-                }.build()
-
-            Image(
-                painter = rememberAsyncImagePainter(R.drawable.splash_screen, imageLoader),
-                contentDescription = null,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-
     }
+
+
     LaunchedEffect(key1 = true) {
         delay(3000);
         if (isUserLoggedIn) {
