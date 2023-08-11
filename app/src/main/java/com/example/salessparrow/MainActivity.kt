ecd6ebe14340b679e27677ad3a952a0b00c12b05
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2023-08-11 11:07:42 +0530

    salesforce connect api fix

--- app/src/main/java/com/example/salessparrow/MainActivity.kt
+++ app/src/main/java/com/example/salessparrow/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.salessparrow
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -36,6 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onResume() {
         super.onResume()
         setContent {
---
+++ app/src/main/java/com/example/salessparrow/data/SalesForceConnectRequest.kt
@@ -0,0 +1,6 @@
+package com.example.salessparrow.data
+
+data class SalesForceConnectRequest(
+    val code: String,
+    val redirect_uri: String
+)
