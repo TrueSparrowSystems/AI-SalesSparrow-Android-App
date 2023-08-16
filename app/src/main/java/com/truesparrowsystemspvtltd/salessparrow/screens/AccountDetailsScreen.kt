c8be8b892cb3784e967221180a4b688b327442b6
Abujdnyaneshwari <84312485+Abujdnyaneshwari@users.noreply.github.com>
2023-08-16 11:27:21 +0530

    Fixed the Loader position to horizontally center of the screen

--- app/src/main/java/com/truesparrowsystemspvtltd/salessparrow/screens/AccountDetailsScreen.kt
+++ app/src/main/java/com/truesparrowsystemspvtltd/salessparrow/screens/AccountDetailsScreen.kt
@@ -6,12 +6,15 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
@@ -138,11 +141,17 @@ fun AccountDetails(
 
 @Composable
 fun Loader() {
-    CircularProgressIndicator(
+    Box(
         modifier = Modifier
-            .height(20.dp)
-            .width(20.dp)
-    )
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(20.dp)
+                .height(20.dp)
+        )
+    }
 }
 
 
