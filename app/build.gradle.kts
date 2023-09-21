b7c2814c38485d82b51d534838db9b7d66880129
dakshbhardwaj <dakshbhardwaj2@gmail.com>
2023-09-21 20:34:04 +0530

    increased ibv

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ android {
         }
         create("staging") {
             applicationIdSuffix = ".dev"
-            versionCode = 7
+            versionCode = 8
             versionName = "1.0.0"
             resValue("string", "app_name", "Sales-Dev")
             buildConfigField(
